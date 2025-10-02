3b1bf52391b55ddee8900485ad9b535600206734
Anthony Uribe <anthonyuribe3456@gmail.com>
2025-10-02 00:59:52 -0600

    PWA sat up 2.0.1

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // service-worker.js - Service Worker para PWA con caché offline
-const CACHE_NAME = "mcbeditor-v1.0.0";
+const CACHE_NAME = "mcbeditor-v2.0.3";
 const RUNTIME_CACHE = "mcbeditor-runtime-v1";
 const TYPES_CACHE = "mcbeditor-types-v1";
 
