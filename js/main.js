f9ccdbfba3f89f52cc0c245f470603cf4df46b67
Anthony Uribe <anthonyuribe3456@gmail.com>
2025-10-18 14:55:34 -0600

    Version 1.4.0.6, sw improvements

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // ===== CONSTANTES GLOBALES =====
 const APP_CONFIG = {
-    VERSION: '1.4.0.5',
+    VERSION: '1.4.0.6',
     STORAGE_KEYS: {
         CONTENT: 'mcbe_editor_content',
         FILENAME: 'mcbe_editor_filename',
